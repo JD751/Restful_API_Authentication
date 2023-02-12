c74695b40c99944196a750118706554d30a95bdc
Jawad Ali <jawad751@gmail.com>
2023-02-12 16:58:14 +0100

    Django_project_creation

--- API_app/settings.py
+++ API_app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sb=g6njbdzx+-$nrdo_sk+0ik!ro#57c(mut14w0k6v7no_j)2'
+SECRET_KEY = os.getenv('Django_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
